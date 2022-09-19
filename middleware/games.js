e9f14b13e52c5ac3e10776abec01887a942d1170
Micah Jackson <thehazelwize@gmail.com>
2022-09-19 18:31:02 -0400

    Added fix for games being added twice. Fixed hours input less than 1 being allowed.

--- middleware/games.js
+++ middleware/games.js
@@ -24,5 +24,30 @@ module.exports = {
         }
         
     },
+    checkGameAdded: async(req,res,next) =>{
+        try{
+            let hasGame = await Game.findOne({slug: req.body.slug, userId: req.user._id})
+            if(hasGame){
+                res.redirect(`/games/profile/added/${req.body.slug}`)
+            }else{
+                next()
+            }
+        }
+        catch(err){
+            console.log(err)
+        }
+    },
+    checkHours: async(req,res,next) => {
+        try{
+            if(+req.body.hoursPlayed < 1){
+                res.redirect(`/games/profile/${req.body.slug}`)
+            }else{
+                return next()
+            }
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
 
 }
--- routes/games.js
+++ routes/games.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const {ensureAuth, guestAuth} = require('../middleware/auth')
-const {checkGameName, checkHasGame} = require('../middleware/games')
+const {checkGameName, checkHasGame, checkGameAdded, checkHours} = require('../middleware/games')
 const gamesController = require('../controllers/games')
 
 router.get('/search', ensureAuth, checkGameName, gamesController.searchGames)
 router.get('/profile/:gameName', checkHasGame, gamesController.getGameProfile)
-router.post('/addGame', gamesController.addGame)
+router.post('/addGame', checkGameAdded, checkHours, gamesController.addGame)
 router.get('/profile/added/:gameName', gamesController.getGameFriendList)
 
 module.exports = router
